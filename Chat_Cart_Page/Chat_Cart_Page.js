992fbd23b804c508f6fd150eed078214fb8c273c
Mohd Danish <113216768+Danish0611@users.noreply.github.com>
2023-04-02 20:21:32 +0530

    Update Chat_Cart_Page.js

--- Chat_Cart_Page/Chat_Cart_Page.js
+++ Chat_Cart_Page/Chat_Cart_Page.js
@@ -74,7 +74,7 @@ for(let a= 0; a < cart.length; a++)
     sum += cart[a].prices * cart[a].quantity;
 }
 amount.textContent = sum;
-totalAmount.textContent = sum;
+totalAmount.textContent = sum - 500;
 }
 
 let searchInp = document.getElementById("search")
@@ -96,33 +96,3 @@ displayData(filteredData)
 }
 
 })
-
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
